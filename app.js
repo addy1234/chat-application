04324b572f22419c82ef2e85a5e62bf93d5451fa
addy1234 <aditya123garg@gmail.com>
2020-09-02 19:09:42 +0530

    adding login functionalities

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 const express = require("express");
 const socket = require("socket.io");
 const mysql = require('mysql');
+const passport = require('passport');
+const configPassport = require('./config/configPassport');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "addy1234",
-  password: "1234",
-  database: "chat_app"
+configPassport(passport);
+app.use(require('express-session')
+({    
+      secret: "this is first site I'm gonna Deploy",
+      resave: false,
+      saveUninitialized: false 
+}));  
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post('/signup', passport.authenticate('local-signup'), function(req, res){
+  
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+app.post('/login', passport.authenticate('local-login'), userResponse);
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "addy1234",
+//   password: "1234",
+//   database: "chat_app"
+// });
+
+// Securities provided by passport.js
+
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
 
 // var sql_query = "insert into message (sender_id, receiver_id, mssg) values (1, 3, 'Hello')";
-con.query(sql_query, function(data){
-  console.log(data);
-})
+// con.query(sql_query, function(data){
+//   console.log(data);
+// })
 
 // Add Google authentication as well.
 
@@ -35,9 +58,10 @@ const app = express();
 
 // Add login functionality
 
-
+app.set("view engine", "ejs");
 // Static files
 app.use(express.static("public"));
+
 
 const server = app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
@@ -54,4 +78,13 @@ io.on("connection", function (socket) {
       socket.broadcast.emit("res", data);
     });
 });
+
+app.get('/', function(req, res){
+  res.render('index.ejs');
+});
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
 
