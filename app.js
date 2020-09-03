53be849e8600e09a994da6189d73a2c8f7db7dad
addy1234 <aditya123garg@gmail.com>
2020-09-03 21:07:30 +0530

    removing passport.js support and adding manual login support

--- app.js
+++ app.js
@@ -2,40 +2,22 @@ const express = require("express");
 const socket = require("socket.io");
 const mysql = require('mysql');
 const passport = require('passport');
-const configPassport = require('./config/configPassport');
+const bodyParser = require('body-parser');
 
-configPassport(passport);
-app.use(require('express-session')
-({    
-      secret: "this is first site I'm gonna Deploy",
-      resave: false,
-      saveUninitialized: false 
-}));  
-
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.post('/signup', passport.authenticate('local-signup'), function(req, res){
-  
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "addy1234",
+  password: "1234",
+  database: "chat_app"
 });
-
-app.post('/login', passport.authenticate('local-login'), userResponse);
-
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "addy1234",
-//   password: "1234",
-//   database: "chat_app"
-// });
 
 // Securities provided by passport.js
 
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Database Connected!");
+});
 
 // var sql_query = "insert into message (sender_id, receiver_id, mssg) values (1, 3, 'Hello')";
 // con.query(sql_query, function(data){
@@ -43,6 +25,8 @@ app.post('/login', passport.authenticate('local-login'), userResponse);
 // })
 
 // Add Google authentication as well.
+// Learn about sessions in detail. (secret)
+// Different types of sessions.
 
 
 // App setup
@@ -61,7 +45,8 @@ const app = express();
 app.set("view engine", "ejs");
 // Static files
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const server = app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
@@ -87,4 +72,64 @@ app.get('/login', function(req, res) {
   res.render('login');
 });
 
+app.get('/signup', function(req, res) {
+  res.render('signup');
+});
 
+app.post('/login', function(req, res) {
+  let userData =  req.body;
+  console.log(userData);
+  let userEmail = userData.email;
+  let userPassword = userData.password;
+
+  sqlQuery = `select * from users where email='${userEmail}' and password='${userPassword}}'`;
+  con.query(sqlQuery, function(err, data) {
+    if (err) {
+      console.log(err); 
+    } else {
+        if (data.length == 0) {
+          console.log('No user found');
+          res.redirect('/signup');
+        } else {
+          res.redirect('/');
+        }
+    }
+  });
+});
+
+app.post('/signup', function(req, res) {
+  let userData =  req.body;
+  console.log(userData);
+  let userEmail = userData.email;
+  let userPassword = userData.password;
+
+  // Try using exists in sql.
+  sqlQuery = `select * from users where email='${userEmail}'`;
+  // sqlQuery2 = `insert into users (email, password) values ('${userEmail}', '${userPassword}')`;
+  con.query(sqlQuery, function(err, data){
+    if(err) {
+      // Add flash message to the users to convery tumse na ho payega.
+      console.log(err);
+    } else {
+      console.log(data);
+      console.log("Query Executed Successfully");
+      if (data.length == 0) {
+        // Register user here.
+        sqlRegisterUserQuery = `insert into users (email, password) values ('${userEmail}', '${userPassword}')`;
+        con.query(sqlRegisterUserQuery, function(err, data) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(data);
+            console.log(`User inserted in users table`);
+            res.redirect('/login');
+          }
+        });
+      } else {
+        // render not working here. why?
+        res.redirect('/login');
+      }
+    }
+  })
+
+});
